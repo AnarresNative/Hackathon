452a14ad68e59da956b1e3ec92c3757635ffb280
AnarresNative <louisdebnam@proton.me>
2024-11-19 13:37:25 +0000

    Set up audio streaming using SoundDevice and Matplotlib to graph

--- Test.py
+++ Test.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import argparse
+import queue
+import sys
+
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import numpy as np
+import sounddevice as sd
+
